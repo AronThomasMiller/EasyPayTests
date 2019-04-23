2a86d274cf7b35c630e998cc9be46e46afaa81f9
Nafania <nafania578@gmail.com>
2019-04-24 01:15:56 +0300

    Juiice

---
+++ EasyPayTests/UserTest.cs
@@ -0,0 +1,33 @@
+﻿using EasyPayLibrary;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPayTests
+{
+    public class UserTest
+    {
+        DriverWrapper driver;
+        [SetUp]
+        public void PreCondition()
+        {
+            driver = new DriverFactory().GetDriver();
+            driver.Maximaze();
+        }
+        [Test]
+        public void Test()
+        {
+        }
+
+        [TearDown]
+        public void PostCondition()
+        {
+            driver.Quit();
+        }
+    }
+}
